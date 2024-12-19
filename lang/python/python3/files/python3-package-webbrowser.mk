2c21a8cad884a1acfdb27e563e20500e55f4ad35
esaaprillia <esaapriliasalsabila@gmail.com>
2024-12-19 17:19:50 +0800

    Update python3-package-webbrowser.mk

--- lang/python/python3/files/python3-package-webbrowser.mk
+++ lang/python/python3/files/python3-package-webbrowser.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2020 Josef Schlehofer <pepe.schlehofer@gmail.com>
+# Copyright (C) 2006-2016 OpenWrt.org
 #
 # This is free software, licensed under the GNU General Public License v2.
 # See /LICENSE for more information.
@@ -7,8 +7,14 @@
 
 define Package/python3-webbrowser
 $(call Package/python3/Default)
-  TITLE:=Python $(PYTHON3_VERSION) Web-browser controller
+  TITLE+= Python web-browser controller
   DEPENDS:=+python3-light
+endef
+
+define Package/python3-webbrowser/description
+$(call Package/python3/Default/description)
+
+Convenient web-browser controller
 endef
 
 $(eval $(call Py3BasePackage,python3-webbrowser, \
