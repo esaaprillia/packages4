7ba9ba2d517925673800a5d21d2ceac64de8bf6a
esaaprillia <esaapriliasalsabila@gmail.com>
2025-02-07 18:07:29 +0800

    add python

--- lang/python/meson/meson.mk
+++ lang/python/meson/meson.mk
@@ -97,6 +97,7 @@ endef
 
 define Host/Configure/Meson
 	$(call Meson/CreateNativeFile,$(HOST_BUILD_DIR)/openwrt-native.txt)
+	$(HOST_PYTHON3_VARS) \
 	$(call Meson, \
 		setup \
 		--native-file $(HOST_BUILD_DIR)/openwrt-native.txt \
@@ -122,6 +123,7 @@ endef
 define Build/Configure/Meson
 	$(call Meson/CreateNativeFile,$(PKG_BUILD_DIR)/openwrt-native.txt)
 	$(call Meson/CreateCrossFile,$(PKG_BUILD_DIR)/openwrt-cross.txt)
+	$(PYTHON3_VARS) \
 	$(call Meson, \
 		setup \
 		--buildtype $(if $(CONFIG_DEBUG),debug,plain) \
