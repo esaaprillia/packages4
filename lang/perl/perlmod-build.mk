336404bebbe20eed5d085ca0d67002a316dc34b8
esaaprillia <esaapriliasalsabila@gmail.com>
2025-02-13 05:56:27 +0800

    Update perlmod-build.mk

--- lang/perl/perlmod-build.mk
+++ lang/perl/perlmod-build.mk
@@ -39,9 +39,8 @@ FLOCK:=$(STAGING_DIR_HOST)/bin/flock
 
 define perlmod/host/relink
 	rm -f $(1)/Makefile.aperl
-	mkdir -p $(1)/perl
 	($(FLOCK) -w 900 9 || { echo perlmod/host/relink: failed to acquire lock; exit 1; }; \
-	    $(1) perl && \
+	    $(MAKE) -C $(1) perl && \
 	    $(INSTALL_BIN) $(1)/perl $(PERL_CMD) && \
 	    $(INSTALL_BIN) $(1)/perl $(STAGING_DIR_HOSTPKG)/usr/bin/perl \
 	) 9> $(TMP_DIR)/.perlmod-perl.flock
@@ -50,7 +49,7 @@ endef
 define perlmod/host/Configure
 	(cd $(HOST_BUILD_DIR); \
 	$(FLOCK) -s -w 300 9 || { echo perlmod/host/Configure: failed to acquire lock; exit 1; }; \
-	PERL_MM_USE_DEFAULT=1 \
+	PERL_MM_USE_DEFAULT=0 \
 	$(2) \
 	$(PERL_CMD) Build.PL \
 		$(1) \
@@ -60,7 +59,7 @@ endef
 define perlmod/host/Compile
 	($(FLOCK) -s -w 300 9 || { echo perlmod/host/Compile: failed to acquire lock; exit 1; }; \
 	$(2) \
-	cd $(HOST_BUILD_DIR) && ./Build \
+	$(HOST_BUILD_DIR)/Build \
 		$(1) \
 		install \
 	) 9> $(TMP_DIR)/.perlmod-perl.flock
@@ -69,7 +68,7 @@ endef
 define perlmod/host/Install
 	($(FLOCK) -s -w 300 9 || { echo perlmod/host/Install: failed to acquire lock; exit 1; }; \
 	$(2) \
-	cd $(HOST_BUILD_DIR) && ./Build \
+	$(HOST_BUILD_DIR)/Build \
 		$(1) \
 		install \
 	) 9> $(TMP_DIR)/.perlmod-perl.flock
