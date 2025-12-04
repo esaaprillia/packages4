908875a11946d045b753e9151ac55d0745023c2f
esaaprillia <esaapriliasalsabila@gmail.com>
2025-12-05 01:52:14 +0800

    Update H5f90i_gen.h

--- hdf5/files/H5match_types/H5f90i_gen.h
+++ hdf5/files/H5match_types/H5f90i_gen.h
@@ -29,7 +29,6 @@ typedef struct {c_int_8 a; c_int_8 b;} c_int_16;
 #define c_float_4 float
 #define c_float_8 double
 #define c_float_10 long double
-#define c_float_16 __float128
 
 typedef c_int_8 haddr_t_f;
 typedef c_hsize_t_8 hsize_t_f;
@@ -41,7 +40,6 @@ typedef c_int_4 int_f;
 typedef c_float_4 real_C_FLOAT_f;
 typedef c_float_8 real_C_DOUBLE_f;
 typedef c_float_10 real_C_LONG_DOUBLE_f;
-typedef c_float_16 real_C_FLOAT128_f;
 typedef c_int_8 hid_t_f;
 typedef c_float_4 real_f;
 typedef c_float_8 double_f;
