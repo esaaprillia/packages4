f43646a51cc62cc8c2fbb83f50dc4e336bf6a452
esaaprillia <esaapriliasalsabila@gmail.com>
2026-02-06 07:03:45 +0800

    Update perlmod.mk

--- lang/perl/perlmod.mk
+++ lang/perl/perlmod.mk
@@ -94,7 +94,7 @@ define perlmod/Configure
 		FULL_AR=$(GNU_TARGET_NAME)-ar \
 		LD=$(GNU_TARGET_NAME)-gcc \
 		LDDLFLAGS="-shared -rdynamic $(TARGET_LDFLAGS)"  \
-		LDFLAGS="$(EXTRA_LIBDIRS:%=-L%) $(EXTRA_LIBS:%=-l%) -fPIC" \
+		LDFLAGS="$(EXTRA_LIBDIRS:%=-L%) $(EXTRA_LIBS:%=-l%) " \
 		LIBC=" " \
 		LIB_EXT=.a \
 		OBJ_EXT=.o \
