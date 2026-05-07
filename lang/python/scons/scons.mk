28f3c2d5cfba0ac3ad95492c8887317899c3e759
esaaprillia <esaapriliasalsabila@gmail.com>
2026-05-08 07:03:36 +0800

    Update scons.mk

--- lang/python/scons/scons.mk
+++ lang/python/scons/scons.mk
@@ -16,9 +16,19 @@ define Build/Configure/Default
 			CC="$(TARGET_CROSS)gcc" \
 			CXX="$(TARGET_CROSS)g++" \
 			OBJCOPY="$(TARGET_CROSS)objcopy" \
+			RANLIB="$(TARGET_CROSS)ranlib" \
 			CFLAGS="$(TARGET_CFLAGS) $(EXTRA_CFLAGS)" \
+			CCFLAGS="$(TARGET_CFLAGS) $(EXTRA_CFLAGS)" \
 			CXXFLAGS="$(TARGET_CXXFLAGS) $(EXTRA_CXXFLAGS)" \
 			LINKFLAGS="$(TARGET_LDFLAGS)" \
+			cc="$(TARGET_CROSS)gcc" \
+			cxx="$(TARGET_CROSS)g++" \
+			objcopy="$(TARGET_CROSS)objcopy" \
+			ranlib="$(TARGET_CROSS)ranlib" \
+			cflags="$(TARGET_CFLAGS) $(EXTRA_CFLAGS)" \
+			ccflags="$(TARGET_CFLAGS) $(EXTRA_CFLAGS)" \
+			cxxflags="$(TARGET_CXXFLAGS) $(EXTRA_CXXFLAGS)" \
+			linkflags="$(TARGET_LDFLAGS)" \
 			DESTDIR="$(PKG_INSTALL_DIR)" \
 			$(SCONS_OPTIONS) \
 		install-core \
